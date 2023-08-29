89705fcae297eaf305771774b54de6fea6f39179
Lucas Costa Marques <124885548+LucasCostaMrq@users.noreply.github.com>
2023-08-29 17:17:50 -0300

    Add files via upload

---
+++ Versão 2.0/ProjetoCalculadora2.0/CalculadoraFaseBeta.js
@@ -0,0 +1,38 @@
+let numberOne = document.getElementById("NumberOne")
+let numberTwo = document.getElementById("NumberTwo")
+
+function Sum(){
+    let some = document.getElementById("answer")
+    let result = parseFloat(numberOne.value) + parseFloat(numberTwo.value)
+    some.innerHTML = `A soma dos números resulta em: ${result}`
+}
+
+function Subtraction(){
+    let some = document.getElementById("answer")
+    let result = parseFloat(numberOne.value) - parseFloat(numberTwo.value)
+    some.innerHTML = `A subtração dos números resulta em: ${result}`
+}
+
+function Multiplication(){
+    let some = document.getElementById("answer")
+    let result = parseFloat(numberOne.value) * parseFloat(numberTwo.value)
+    some.innerHTML = `A multiplicação dos números resulta em: ${result}`
+}
+
+function Division(){
+    let some = document.getElementById("answer")
+    let result = parseFloat(numberOne.value) / parseFloat(numberTwo.value)
+    some.innerHTML = `A divisão dos números resulta em: ${result}`
+}
+
+function Division(){
+    let some = document.getElementById("answer")
+    let result = parseFloat(numberOne.value) / parseFloat(numberTwo.value)
+    some.innerHTML = `A divisão dos números resulta em: ${result}`
+}
+
+function Exponent(){
+    let some = document.getElementById("answer")
+    let result = parseFloat(numberOne.value) ** parseFloat(numberTwo.value)
+    some.innerHTML = `A exponenciação do primeiro número elevado ao segundo resulta em: ${result}` 
+}
